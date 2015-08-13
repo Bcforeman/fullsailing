3783e814d6d32486472aeecb886610eaede09923
Bcforeman <bryan.foreman5@gmail.com>
2015-08-13 15:23:27 -0400

    first pass at changes

---
+++ Foreman_Bryan_Expressions_Assignment/Foreman_Bryan_Expressions_Assignment.js
@@ -0,0 +1,32 @@
+/*
+ Bryan Foreman
+ Mobile Development
+ Aug 13th 2015
+ */
+
+//height of forged sword
+var swordHeight;
+//width of forged sword
+var swordWidth;
+//depth of forged sword
+var swordDepth;
+//length of leather shoulder strap in inches
+var strapLength;
+//width of leather shoulder strap in inches
+var strapWidth;
+
+strapLength = 22;
+strapWidth = 2;
+
+//height prompt of sword
+swordHeight = prompt("Enter height of sword forged in inches: ");
+//width prompt of sword
+swordWidth = prompt("Enter width of sword forged in inches: ");
+//depth prompt of sword
+swordDepth = prompt("Enter depth of sword forged in inches: ");
+
+//area squared of leather shoulder strap
+var areaStrap = strapLength * strapWidth ;
+
+
+
